a945c039e7244b5fe830f1cc520257e4b774f615
Jason  Montes <jasone.montes@gmail.com>
2019-04-24 05:29:37 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,23 @@
 require 'pry'
 
 def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
+   i = 0
+   collection = []
+   while i < array.length
+   binding.pry 
+     collection << yield(array[i])
     i += 1
   end
-end
+  collection
+  end
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
+#there are no lights that say passed local test only submit pull request
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+
+
+
